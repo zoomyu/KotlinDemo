bb25601456a7b5ea798c215ef40db1b1dce74ef6
俞威 <zhengyuw2010@126.com>
2017-06-07 18:36:57 +0800

    Use RecyclerView

---
+++ app/src/main/java/com/zoomyu/kotlindemo/ForecastListAdapter.kt
@@ -0,0 +1,26 @@
+package com.zoomyu.kotlindemo
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * Created by yuwei on 17-6-7.
+ */
+
+class ForecastListAdapter(val items: List<String>) :
+        RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(TextView(parent.context))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+
+}
+
--- app/src/main/java/com/zoomyu/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/zoomyu/kotlindemo/MainActivity.kt
@@ -2,13 +2,26 @@ package com.zoomyu.kotlindemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val items = listOf(
+            "Mon	6/23	-	Sunny	-	31/17",
+            "Tue	6/24	-	Foggy	-	21/8",
+            "Wed	6/25	-	Cloudy	-	22/17",
+            "Thurs	6/26	-	Rainy	-	18/11",
+            "Fri	6/27	-	Foggy	-	21/10",
+            "Sat	6/28	-	TRAPPED	IN	WEATHERSTATION	-	23/18",
+            "Sun	6/29	-	Sunny	-	20/7"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        message.text = "Hello Kotlin"
+
+        val forecast_list = findViewById(R.id.forecast_list) as RecyclerView
+        forecast_list.layoutManager = LinearLayoutManager(this)
+        forecast_list.adapter = ForecastListAdapter(items)
     }
 }
